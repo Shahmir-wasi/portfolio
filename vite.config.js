0872657bbf543e52fec274379f6d9c70a74449b7
Shahmir <syedshahmirwasi1@gmail.com>
2025-07-18 01:16:44 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/salta.io/', // 👈 Must match your repo name
+  base: '/portfolio/' // ← exact new repo name here
 });
